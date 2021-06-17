dd45fa12b68c79c190564dc74f9143fc53237d6d
Mar10HT <mherrerajt12@gmail.com>
2021-06-16 22:08:17 -0600

    Prueba

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Millenium Database</Text>
+      <Text>Test</Text>
       <StatusBar style="auto" />
     </View>
   );
