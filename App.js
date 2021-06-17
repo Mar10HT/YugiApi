d5a6168e60b17c4d6177a5c809d278d0c119cc3e
damendoza5 <damendoza97@gmail.com>
2021-06-16 22:11:23 -0600

    Prueba-change

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Millenium Database</Text>
-      <Text>Test</Text>
+      <Text>Test-Change</Text>
       <StatusBar style="auto" />
     </View>
   );
