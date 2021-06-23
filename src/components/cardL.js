cc3f026b0b2869d86b74b8ce5d71aa3cba3faebb
Mar10HT <mherrerajt12@gmail.com>
2021-06-22 20:45:58 -0600

    Cambio

--- src/components/cardL.js
+++ src/components/cardL.js
@@ -6,9 +6,9 @@ const CardList = ({card}) =>{
 
     return(
         <View>
-            {card.meta && card.data.map((card) => {
+            {card.meta && card.data.map((card, index) => {
                 return <Card id = {card.id} name={card.name}  
-                type = {card.type} race = {card.race} image={card.id} key = {card.id}/>;
+                type = {card.type} race = {card.race} image={card.id} key = {index}/>;
             })}
         </View>
     )
